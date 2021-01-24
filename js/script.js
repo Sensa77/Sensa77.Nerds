b4e2b106faad942583c1d6191a885e1f6f84baca
Sensa <77754618083@mail.ru>
2021-01-25 00:03:14 +0300

    Исправлены ошибки, изменена тень, всякие мелочи.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const writeUsModal = document.querySelector(".write-us");
 const writeUsButton = document.querySelector("#write-us-button");
 const writeUsClose = document.querySelector("#write-us-close");
-
+console.log('123');
 const showModal = (event) => {
   event.preventDefault();
   writeUsModal.classList.add("write-us--show");
@@ -12,5 +12,6 @@ const closeModal = (event) => {
     writeUsModal.classList.remove("write-us--show");
 }
 
+console.log(writeUsButton);
 writeUsButton.addEventListener("click", showModal);
 writeUsClose.addEventListener("click", closeModal);
