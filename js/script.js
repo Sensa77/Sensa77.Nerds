4239109044f83f121a14ac14dd4ee49a225f2e85
Sensa <77754618083@mail.ru>
2021-02-06 14:36:30 +0300

    проект закончен

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const writeUsModal = document.querySelector(".write-us");
 const writeUsButton = document.querySelector("#write-us-button");
 const writeUsClose = document.querySelector("#write-us-close");
-console.log('123');
+
 const showModal = (event) => {
   event.preventDefault();
   writeUsModal.classList.add("write-us--show");
@@ -12,6 +12,44 @@ const closeModal = (event) => {
     writeUsModal.classList.remove("write-us--show");
 }
 
-console.log(writeUsButton);
 writeUsButton.addEventListener("click", showModal);
 writeUsClose.addEventListener("click", closeModal);
+
+const sliders = document.querySelectorAll(".slider-block");
+const sliderForm = document.querySelector(".slider-radio");
+
+sliderForm.addEventListener("change", (event) => {
+
+  sliders.forEach((slider) => {
+
+    if(slider.classList.contains("slider-block--show")) {
+      slider.classList.remove("slider-block--show");
+    }
+  });
+  sliders[event.target.value].classList.add("slider-block--show");
+});
+
+
+const writeUsForm = document.querySelector("#write-us-form");
+const writeUsInputs = document.querySelectorAll(".write-us-input");
+const writeUsValidation = (event) => {
+  event.preventDefault();
+  const textErrors = document.querySelectorAll(".text--error");
+  textErrors.forEach( (textError) => {
+    textError.remove();
+  })
+  writeUsInputs.forEach( (input) => {
+    if (!input.value) {
+      input.classList.add("write-us--error");
+      const textError = document.createElement("span");
+      textError.classList.add("text--error");
+      textError.textContent = "Введите что-нибудь!";
+      input.parentElement.append(textError); 
+    } else {
+      input.classList.remove("write-us--error");
+    }
+  })
+}
+
+
+writeUsForm.addEventListener("submit", writeUsValidation);
